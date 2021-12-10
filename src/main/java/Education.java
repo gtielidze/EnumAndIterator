606bbb2b0cdd581940ce91d57a7656bd0587fc59
gio <gtielidze@gmail.com>
2021-12-10 22:01:37 +0400

    Initial commit

---
+++ src/main/java/Education.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gio
+ */
+public enum Education {
+    PHD("Doctoral degree"),
+    MA("Masters degree"),
+    BA("Bachelors degree"),
+    HS("High School diploma");
+
+    private String code;
+
+    private Education(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+}
---
+++ src/main/java/Employees.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author gio
+ */
+public class Employees {
+
+    private ArrayList<Person> employees;
+
+    public Employees() {
+        this.employees = new ArrayList();
+    }
+
+    public void add(Person personToAdd) {
+        employees.add(personToAdd);
+    }
+
+    public void add(List<Person> peopleToAdd) {
+        this.employees.addAll(peopleToAdd);
+    }
+
+    public void print() {
+        Iterator<Person> iterator = this.employees.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public void print(Education education) {
+        Iterator<Person> iterator = this.employees.iterator();
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            if (person.getEducation().ordinal() == education.ordinal()) {
+                System.out.println(person);
+            }
+        }
+    }
+
+    public void fire(Education education) {
+        Iterator<Person> iterator = this.employees.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getEducation().getCode().equals(education.getCode())) {
+                iterator.remove();
+            }
+        }
+    }
+}
